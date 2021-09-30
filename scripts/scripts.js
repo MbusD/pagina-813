cfda9a83aeba49636c50cb6b1d296a909a21263d
MbusD <91702309+MbusD@users.noreply.github.com>
2021-09-30 19:02:34 -0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu')
+
+let menu_bar = document.querySelector('#menubar')
+
+menu_bar.addEventListener('click', function() {
+
+    menu.classList.toggle('menu-toggle');
+    
+})
